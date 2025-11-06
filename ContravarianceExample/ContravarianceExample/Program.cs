c512a8f9dd1619ccf7b614c8b6aabd166bbad3ce
YKJ <vickykat2468@gmail.com>
2025-11-06 22:10:23 +0300

    Create contravariance example

---
+++ ContravarianceExample/ContravarianceExample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ContravarianceExample
+{
+    public interface IMyList<in T>
+    {
+        void PrintElementType(T element);
+    }
+
+    public class MyList<T> : IMyList<T> 
+    {
+        public void PrintElementType(T element)
+        {
+            Console.WriteLine(element.GetType().Name); // Skeleton
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IMyList<Skeleton> myList = new MyList<Monster>();
+
+            myList.PrintElementType(new Skeleton());
+            Execute(new MyList<Monster>());
+        }
+
+        public static void Execute(IMyList<Skeleton> monsters) { }
+    }
+
+    public class Monster { }
+
+    public class Skeleton : Monster { }
+}
